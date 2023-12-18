665f4ca549848627f0b122c4eb2a530b065e4847
mezue30000 <mezue3000>
2023-12-18 03:31:04 +0100

    Generated moves/ stored in local-storage

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// Update to local storage
+   const score =  JSON.parse(localStorage.getItem('score'));
+
 // create a function to generate flips using Math.random
 function pickMove (playermove) {
     
@@ -6,24 +9,82 @@ function pickMove (playermove) {
     let picker = ''
     
          if (randNum >= 0 && randNum < 1/3) {
-               picker = 'orange'
+               picker = 'lemon'
          } else if (randNum >= 1/3 && randNum < 2/3) {
-              picker = 'banana'
+              picker = 'apple'
          } else if (randNum >= 2/3 && randNum < 1) {
-              picker = 'apple';
+              picker = 'strawberry';
     
          }
-    // Generating moves for each fruit flip
+    // Generating moves for each fruit flips
         let result = ''
     
-         if (playermove === 'orange') {
-            if (picker === 'orange') {
-                result = 'tie'
-           } else if (picker === 'banana') {
-                result = 'loss'
+         if (playermove === 'lemon') {
+            if (picker === 'lemon') {
+                result = 'you tie'
            } else if (picker === 'apple') {
-               result = 'you win'
+                result = 'you win'
+           } else if (picker === 'strawberry') {
+               result = 'you loss'
            }
      
          }
-};
+
+
+         if (playermove === 'apple') {
+            if (picker === 'lemon') {
+                result = ' you win'
+           } else if (picker === 'apple') {
+                result = 'you tie'
+           } else if (picker === 'strawberry') {
+               result = 'you win'
+           }
+        
+         }
+
+
+         if (playermove === 'strawberry') {
+            if (picker === 'lemon') {
+                result = 'you loss'
+           } else if (picker === 'apple') {
+                result = 'you win'
+           } else if (picker === 'strawberry') {
+               result = 'you tie'
+           }
+        
+         }
+
+
+         if (result === 'you win') {
+            score.wins++
+       } else if (result === 'you loss') {
+            score.losses++
+       } else if (result === 'you tie') {
+            score.ties++
+       }
+       
+    //convert js-object to json string
+       const jsonFile = JSON.stringify(score);
+
+    //Save to local storage    
+       localStorage.setItem('score', jsonFile);
+  
+   //Update the result    
+       stephy.innerHTML = `computermove <img src="Images/${picker}.jpg" class="img">
+       <img src="Images/${playermove}.jpg" class="img"> player ${result}-------------------------------
+       wins:${score.wins}, losses:${score.losses}, ties:${score.ties}`
+
+};   
+
+    // create function for the reset button  
+     function reset () {
+         score.wins = 0,
+         score.losses = 0,
+         score.ties  = 0
+
+
+     }
+
+    //  Grab the Paragraph element
+        const stephy = document.querySelector('.js-para')
+        stephy.innerHTML = `computerMove = ${picker},  playermove = ${playermove}`
